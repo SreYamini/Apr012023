8881772132877e465181a143d5d9d0530d6a0dab
SreYamini <75999159+SreYamini@users.noreply.github.com>
2023-04-02 20:53:43 +0530

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		int age = 27;
+		boolean isbreafastover = true;
+		String name = "Sre Yamini";
+		
+		System.out.println("The name is "+name+" with age "+age+" isbreafastover "+isbreafastover);
+	}
+}
